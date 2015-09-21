db571775e0b93b0b269f41b46c93ad47937fca20
oleg.pahhomov@gmail.com <lol98nuj>
2015-09-21 14:38:14 +0300

    m

--- src/main/java/config/AppDataSource.java
+++ src/main/java/config/AppDataSource.java
@@ -7,18 +7,29 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 public class AppDataSource {
-    private static PGPoolingDataSource dataSource;
+    //private static DataSource dataSource = initLocalDbSource();
+    private static DataSource dataSource = initHerokuDbsource();
     public static final String HOME_PW = "12345";
     public static final String OFFICE_PW = "postgres";
 
-    static {
-        // Not recommended, you may need to implement connection pooling or use apache database pooling lib
-        dataSource = new PGPoolingDataSource();
+
+    private static DataSource initHerokuDbsource() {
+        PGPoolingDataSource dataSource = new PGPoolingDataSource();
+        dataSource.setUser("tiksmuyqdctiyq");
+        dataSource.setPassword("okiquQB9YLY-XaoMoKEVuaVNSC");
+        dataSource.setServerName("@ec2-54-197-253-142.compute-1.amazonaws.com");
+        dataSource.setDatabaseName("dbcanrndvorfiu");
+        return dataSource;
+    }
+
+    private static DataSource initLocalDbSource() {
+        PGPoolingDataSource dataSource = new PGPoolingDataSource();
         dataSource.setDatabaseName("postgres");
         dataSource.setUser("postgres");
 //        dataSource.setPassword(HOME_PW);
         dataSource.setPassword(OFFICE_PW);
         dataSource.setServerName("localhost");
+        return dataSource;
     }
 
     public static DataSource getDatasource() {
